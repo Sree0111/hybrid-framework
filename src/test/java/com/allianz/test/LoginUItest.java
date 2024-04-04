53420d7534ce8ebfab42aa1932a04e578466c926
Sree0111 <s.sreelakshmi07@gmail.com>
2024-04-03 23:38:12 -0700

    adding 2

--- src/test/java/com/allianz/test/LoginUItest.java
+++ src/test/java/com/allianz/test/LoginUItest.java
@@ -1,17 +1,37 @@
 package com.allianz.test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginUItest {
 
-	@Test(invocationCount = 2)
+	@Test()
 	public void titleTest()
 	{
-		System.out.println("test title******");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		String actualTitle=driver.getTitle();
+		System.out.println("title+++");
+		Assert.assertEquals(actualTitle, "OrnageHRM");
+		
 	}
 	@Test(priority = 1)
 	public void methodTest(){
-		System.out.println("test content*****");
-		System.out.println("test");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php");
+		String actualVersion=driver.findElement(By.xpath("//div[@class='orangehrm-copyright-wrapper']/p[1]")).getText();
+		System.out.println(actualVersion);
+		Assert.assertEquals(actualVersion, "OrangeHRM OS 5.6.1");
 	}
 }
